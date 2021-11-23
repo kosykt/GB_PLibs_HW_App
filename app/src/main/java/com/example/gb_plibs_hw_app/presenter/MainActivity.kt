5eefefa27e1a31d8669ad631a45e6af723befe24
konastantin <kosvasl09@gmail.com>
2021-11-24 03:05:35 +0900

    Initial commit

---
+++ app/src/main/java/com/example/gb_plibs_hw_app/presenter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gb_plibs_hw_app.presenter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gb_plibs_hw_app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
