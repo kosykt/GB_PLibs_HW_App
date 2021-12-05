2b9408d90539e3de7d974042b9e79de361065aca
konstantin <kosvasl09@gmail.com>
2021-12-05 13:26:14 +0900

    переписан фрагмент

--- app/src/main/java/com/example/gb_plibs_hw_app/hw4/ui/fragment/ConvertPictureFragment.kt
+++ app/src/main/java/com/example/gb_plibs_hw_app/hw4/ui/fragment/ConvertPictureFragment.kt
@@ -5,16 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.gb_plibs_hw_app.App
 import com.example.gb_plibs_hw_app.R
+import com.example.gb_plibs_hw_app.databinding.FragmentConvertPictureBinding
+import com.example.gb_plibs_hw_app.databinding.FragmentLoginBinding
+import com.example.gb_plibs_hw_app.ui.base.BackButtonListener
+import com.example.gb_plibs_hw_app.ui.users.UsersView
+import moxy.MvpAppCompatFragment
+import moxy.ktx.moxyPresenter
 
-class ConvertPictureFragment : Fragment() {
+class ConvertPictureFragment : MvpAppCompatFragment(), ConvertPictureView, BackButtonListener {
+
+    private var _binding: FragmentConvertPictureBinding? = null
+    private val binding
+        get() = _binding!!
+
+    private val presenter by moxyPresenter { ConvertPicturePresenter(App.instance.router) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_convert_picture, container, false)
+        _binding = FragmentConvertPictureBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun convertToPNG() {
+        TODO("Not yet implemented")
+    }
+
+    override fun backPressed(): Boolean {
+        presenter.backPressed()
+        return true
     }
 
 }
--- app/src/main/java/com/example/gb_plibs_hw_app/hw4/ui/fragment/ConvertPicturePresenter.kt
+++ app/src/main/java/com/example/gb_plibs_hw_app/hw4/ui/fragment/ConvertPicturePresenter.kt
@@ -1,6 +1,14 @@
 package com.example.gb_plibs_hw_app.hw4.ui.fragment
 
+import com.github.terrakok.cicerone.Router
 import moxy.MvpPresenter
 
-class ConvertPicturePresenter(): MvpPresenter<ConvertPictureView>() {
+class ConvertPicturePresenter(
+    private val router: Router
+): MvpPresenter<ConvertPictureView>() {
+
+    fun backPressed(): Boolean {
+        router.exit()
+        return true
+    }
 }
