438e211670bcda4c8e7881bf3e12c594ef855137
konstantin <kosvasl09@gmail.com>
2021-12-17 17:00:35 +0900

    исправил, был упущен onCreate

--- app/src/main/java/com/example/gb_plibs_hw_app/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/gb_plibs_hw_app/presentation/ui/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gb_plibs_hw_app.presentation.ui.main
 
+import android.os.Bundle
 import com.example.gb_plibs_hw_app.R
 import com.example.gb_plibs_hw_app.presentation.App
 import com.example.gb_plibs_hw_app.presentation.ui.base.BackButtonListener
@@ -20,6 +21,12 @@ class MainActivity : MvpAppCompatActivity(R.layout.activity_main), MainView {
         MainPresenter().apply {
             App.instance.appComponent.inject(this)
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        App.instance.appComponent.inject(this)
     }
 
     override fun onResumeFragments() {
