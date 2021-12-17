3fa5e79a6a937bfaa3e9c1d02c9ffd76613c7b1d
konstantin <kosvasl09@gmail.com>
2021-12-17 12:50:30 +0900

    init

--- app/src/main/java/com/example/gb_plibs_hw_app/presentation/AppScreens.kt
+++ app/src/main/java/com/example/gb_plibs_hw_app/presentation/AppScreens.kt
@@ -2,12 +2,13 @@ package com.example.gb_plibs_hw_app.presentation
 
 import com.example.gb_plibs_hw_app.domain.userdetails.model.UserDetailsModel
 import com.example.gb_plibs_hw_app.domain.users.model.UsersModel
-import com.example.gb_plibs_hw_app.presentation.ui.userdetails.UserDetailsFragment
 import com.example.gb_plibs_hw_app.presentation.ui.repodetails.RepoDetailsFragment
+import com.example.gb_plibs_hw_app.presentation.ui.userdetails.UserDetailsFragment
 import com.example.gb_plibs_hw_app.presentation.ui.users.UsersFragment
 import com.github.terrakok.cicerone.androidx.FragmentScreen
 
 object AppScreens {
+
 
     fun usersScreen() = FragmentScreen {
         UsersFragment()
